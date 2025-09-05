1ad5d54af4923803b3064f2595b387cd59b9e18a
Rontoge <ronaldrg.rr@gmail.com>
2025-09-05 21:55:52 +0200

    start profilr screen

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,} from 'react-native'
 import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
+
 
 const ProfileScreen = () => {
   return (
-    <View>
+    <SafeAreaView className='flex-1 bg-gray-100'>
       <Text>ProfileScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
