4c7f42db269a3a12d8464fc175f5911ca87f99cd
Rontoge <ronaldrg.rr@gmail.com>
2025-09-06 21:33:19 +0200

    backend intioted and connected to mongo db

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require("http");
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://ronald:ronald@cluster0.tenlsqe.mongodb.net/")
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.error("DB Connection Error: "));
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
