24f4d8ae95c24cbdcd07d461ee647aba93ce9a79
Rontoge <ronaldrg.rr@gmail.com>
2025-09-12 23:48:11 +0200

    Fixed upadate issuse

--- screens/NameScreen.tsx
+++ screens/NameScreen.tsx
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const NameScreen = () => {
+  const navigation = useNavigation();
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const handleNext = () => {
+    // if (! firstName.trim()) return;
+    navigation.navigate("Image")
+  }
+
   return (
-    <View>
-      <Text>NameScreen</Text>
+    <View className="flex-1 bg-white px-6 pt-20">
+      <Text className=" text-2xl font-bold mb-4">Lets Get to know you</Text>
+
+      <Text className="text-sm mb-1">First Name *</Text>
+      <TextInput
+        placeholder="Enter First Name"
+        value={firstName}
+        onChangeText={setFirstName}
+        className="border border-gray-300 rounded-xl px-4 py-3 mb-4"
+      />
+
+      
+      <Text className="text-sm mb-1">Last Name (Optional)</Text>
+      <TextInput
+        placeholder="Enter Last Name"
+        value={lastName}
+        onChangeText={setLastName}
+        className="border border-gray-300 rounded-xl px-4 py-3 mb-4"
+      />
+
+
+      <TouchableOpacity onPress={handleNext} className="bg-black py-4 rounded-xl">
+        <Text className="text-white text-center font-bold text-base">
+          Next
+        </Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default NameScreen
+export default NameScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
